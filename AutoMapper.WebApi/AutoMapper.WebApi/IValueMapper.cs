0f5c88ceba81ecf0cbfe593bd08ec30333b4b225
Chris Thomas <xiotox@gmail.com>
2012-09-22 23:26:03 -0400

    Initial Commit

---
+++ AutoMapper.WebApi/AutoMapper.WebApi/IValueMapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMapper.WebApi {
+    public interface IValueMapper<in TSource, TTarget> {
+        TTarget Map(TSource source);
+        void Release(TTarget target);
+    }
+
+    public interface IValueMapper<in TSource> {
+
+        object Map(TSource source, string profile);
+        void Release(object target);
+
+    }
+}
